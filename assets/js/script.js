6f4287c9f32ae265d36ddecadc71ec61bc8782da
Arthur Lins Amaral <112766699+Arthur-Lins-Amaral29@users.noreply.github.com>
2022-12-03 16:25:12 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,96 @@
+/*CONTAGEM DE PONTOS*/
+var contUser = 0
+var contPC = 0
+
+/*ELEMENTOS DA APLICAÇÃO*/
+const imgUser = document.getElementById("user")
+const imgPc = document.getElementById("pc")
+const playing = document.getElementById("playing")
+const contador = document.getElementById("contador")
+const winner = document.getElementById("winner")
+const loser = document.getElementById("loser")
+
+/*SONS*/
+const audioWin = new Audio("assets/sounds/winning.wav")
+const audioLose = new Audio("assets/sounds/losing.wav")
+
+/*VARIÁVEIS DE ELEMENTOS*/
+var player1 = ""
+var player2 = ""
+
+playing.addEventListener('click', () => {
+    reset()
+    playPc()
+})
+
+function reset(){
+    player1 = document.querySelector('input[name="play"]:checked').value
+    imgUser.innerHTML = "<img src='assets/img/" + player1 + ".png'>"
+    imgPc.innerHTML = ""
+}
+
+function playPc(){
+    let opt = ['rock', 'paper', 'scissor']
+    let num = Math.floor(Math.random() * (2 - 0 + 1)) + 0
+    player2 = opt[num]
+    imgPc.innerHTML = "<img src='assets/img/" + player2 + ".png'>"
+    analyze()
+}
+
+function analyze(){
+    playing.disabled = true
+
+    let win = "0"
+    
+    if(player1 == player2){
+
+    } else if(player1 == "rock"){
+        win = player2 == 'scissor' ? 1 : -1
+    } else if(player1 == 'paper'){
+        win = player2 == 'rock' ? 1 : -1 
+    } else if(player1 == 'scissor'){
+        win = player2 == 'paper' ? 1 : -1
+    }
+
+    if(win == 0){
+        
+    } else if(win > 0){
+        contUser = contUser + 1
+        audioWin.play()
+    } else{
+        contPC = contPC + 1
+        audioLose.play()
+    }
+
+    if(contUser >= 5){
+        winner.classList.remove('none')
+        winner.classList.add('center')
+    }
+
+    if(contPC >= 5){
+        loser.classList.remove('none')
+        loser.classList.add('center')        
+    }
+
+    contador.innerHTML = contUser + ":" + contPC
+
+    setTimeout(() =>{
+        playing.disabled = false
+    }, 1000)
+}
+
+function clear(){
+    imgPc.innerHTML = ""
+    imgUser.innerHTML = ""
+}
+
+function newGame(){
+    contador.innerHTML = "0:0"
+    contPC = 0
+    contUser = 0
+    reset()
+    winner.classList.add('none')
+    winner.classList.remove('center')
+    loser.classList.add('none')
+    loser.classList.remove('center')
+}
